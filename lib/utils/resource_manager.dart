c8df15f45edb5533eb34fa284028107d553b47ef
Bonfra04 <bonfract@gmail.com>
2024-08-25 23:29:44 +0200

    resource manager class

---
+++ lib/utils/resource_manager.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:mime/mime.dart';
+import 'package:stronz_video_player/utils/simple_http.dart' as http;
+
+class ResourceManager {
+    static Future<String> type(Uri uri) async {
+        if (uri.scheme == "http" || uri.scheme == "https")
+            return await http.mime(uri);
+
+        return lookupMimeType(uri.toString()) ?? "unknown";
+    }
+
+    static Future<String> content(Uri uri) async {
+        if (uri.scheme == "http" || uri.scheme == "https")
+            return await http.get(uri);
+
+        File file = File.fromUri(uri);
+        return await file.readAsString();
+    }
+}
