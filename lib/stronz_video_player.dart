e3c098d061f64a08b7dcd391c30707daebeea7af
Bonfra04 <bonfract@gmail.com>
2024-09-23 20:43:24 +0200

    exporting useful files

--- lib/stronz_video_player.dart
+++ lib/stronz_video_player.dart
@@ -1,7 +1,9 @@
 library stronz_video_player;
 
 export 'package:stronz_video_player/data/playable.dart';
+export 'package:stronz_video_player/data/tracks.dart';
 export 'package:stronz_video_player/components/stronz_video_player.dart';
 export 'package:stronz_video_player/components/controls/stronz_player_control.dart';
 export 'package:stronz_video_player/logic/controller/stronz_player_controller.dart';
+export 'package:stronz_video_player/logic/controller/stronz_external_controller.dart';
 export 'package:stronz_video_player/data/stronz_controller_state.dart';
