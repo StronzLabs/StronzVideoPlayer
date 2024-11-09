446ec0afc7055bd33d47e8a58772398e9b10b030
Bonfra04 <bonfract@gmail.com>
2024-11-09 16:25:45 +0100

    inherit user agent from HTTP library

--- lib/logic/controller/native_player_controller.dart
+++ lib/logic/controller/native_player_controller.dart
@@ -5,6 +5,7 @@ import 'package:stronz_video_player/data/stronz_controller_state.dart';
 import 'package:stronz_video_player/data/playable.dart';
 import 'package:stronz_video_player/data/tracks.dart';
 import 'package:stronz_video_player/logic/controller/stronz_player_controller.dart';
+import 'package:sutils/sutils.dart';
 import 'package:video_player/video_player.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 
@@ -47,7 +48,9 @@ class NativePlayerController extends StronzPlayerController {
                 throw Exception("Unsupported tracks type: ${this.tracks.runtimeType}");
         }
 
-        return VideoPlayerController.networkUrl(uri);
+        return VideoPlayerController.networkUrl(uri,
+            httpHeaders: {"User-Agent": HTTP.userAgent}
+        );
     }
 
     Future<void> _refreshHLSFile() async {
